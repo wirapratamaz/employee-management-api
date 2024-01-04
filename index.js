1319f7aa007e7b6ffa3ab251c33dab7fce552ae2
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-01-04 10:22:21 +0800

    example swagger

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const karyawanRoutes = require('./routes/karyawan');
 const cutiRoutes = require('./routes/cuti');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Employee Management API',
+      version: '1.0.0',
+      description: 'API for managing employee and leave data',
+      contact: {
+        name: 'Ads Digital'
+      },
+      servers: ['http://localhost:3000']
+    }
+  },
+  // ['.routes/*.js']
+  apis: ['index.js', './routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 
 // Routes
 app.use('/karyawan', karyawanRoutes);
--- routes/cuti.js
+++ routes/cuti.js
@@ -1,17 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cutiController = require('../controllers/cutiController');
+const cutiController = require("../controllers/cutiController");
 
-// Get all leaves
-router.get('/', cutiController.getAllCuti);
+/**
+ * @swagger
+ * /cuti:
+ *   get:
+ *     summary: Get a list of all cuti records
+ *     tags: [GET Cuti]
+ *     responses:
+ *       200:
+ *         description: List of cuti records
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Cuti'
+ *       500:
+ *         description: Server error
+ */
+router.get("/", cutiController.getAllCuti);
 
-// Create a new leave
-router.post('/', cutiController.createCuti);
+/**
+ * @swagger
+ * /cuti:
+ *  post:
+ *      summary: post data cuti
+ *      tags: [POST Cuti]
+ * components:
+ *   schemas:
+ *     Cuti:
+ *       type: object
+ *       required:
+ *         - nomorInduk
+ *         - tanggalCuti
+ *         - lamaCuti
+ *         - keterangan
+ *       properties:
+ *         nomorInduk:
+ *           type: string
+ *           description: The employee ID
+ *           example: "IP06001"
+ *         tanggalCuti:
+ *           type: string
+ *           format: date
+ *           description: The start date of the leave
+ *           example: "2023-01-01"
+ *         lamaCuti:
+ *           type: integer
+ *           description: The duration of the leave in days
+ *           example: 5
+ *         keterangan:
+ *           type: string
+ *           description: Description of the leave
+ *           example: "Annual leave"
+ */
+router.post("/", cutiController.createCuti);
 
 // Update a leave
-router.put('/:id', cutiController.updateCuti);
+router.put("/:id", cutiController.updateCuti);
 
 // Delete a leave
-router.delete('/:id', cutiController.deleteCuti);
+router.delete("/:id", cutiController.deleteCuti);
 
 module.exports = router;
